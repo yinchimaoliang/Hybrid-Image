de7e4c6aa5419a5cec2b213b7e10c7433e5c9f6f
Li Yinhao <liyinhao0413@qq.com>
2019-03-27 16:11:35 +0800

    first commit

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!"D:\OneDrive - mail.nankai.edu.cn\File\南开\大三下\计算机视觉基础\Hybrid Image\venv\Scripts\python.exe" -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
