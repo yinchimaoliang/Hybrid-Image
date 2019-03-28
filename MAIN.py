3ae78aac8dbab894cf18db763c1bc48d7036bd3d
Yinhao Li <liyinhao0413@qq.com>
2019-03-28 22:10:26 +0800

    finish Gaussian

--- MAIN.py
+++ MAIN.py
@@ -1,20 +1,23 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-
+import math
 
 
 SRC1 = "./images/Barbara Palvin.jpg"
-SRC2 = "./images/Lily Donaldson.jpg"
-
+SRC2 = "./images/Constance Jablonski.jpg"
+OUTPUT = "./result/result.jpg"
+RADIUS = 20
+SIGMA = 10
 
 
 class MAIN():
+
+    #图片加载
     def __init__(self,src1,src2):
         self.src1 = src1
         self.src2 = src2
 
-
+    #将两张图片变换为大小一致
     def loadImage(self):
         self.img1 = cv.imread(self.src1)
         self.img2 = cv.imread(self.src2)
@@ -22,17 +25,32 @@ class MAIN():
         y = self.img1.shape[1]
         self.img2 = cv.resize(self.img2,(y,x),interpolation = cv.INTER_CUBIC)
 
-
-    def myFilter(self,sigma):
-        kernel_5x5 = np.array([
-            [1, 4, 7, 4, 1],
-            [4, 16, 26, 16, 4],
-            [7, 26, 41, 26, 7],
-            [4, 16, 26, 16, 4],
-            [1, 4, 7, 4, 1]
-        ])
+    def get_cv(self,r, sigma):
+        return 1 / (2 * math.pi * sigma ** 2) * math.exp((-r ** 2) / (2 * sigma ** 2))
 
 
+
+
+    def getFilter(self,radius,sigma):
+        window = np.zeros((radius * 2 + 1, radius * 2 + 1))
+        for i in range(-radius, radius + 1):
+            for j in range(-radius, radius + 1):
+                r = (i ** 2 + j ** 2) ** 0.5
+                window[i + radius][j + radius] = self.get_cv(r, sigma)
+        return window / np.sum(window)
+
+    def myGaussian(self,src):
+        myFilter = self.getFilter(RADIUS,SIGMA)
+        print(myFilter)
+        # trans = np.zeros(src.shape,np.uint8)
+        print(src)
+        trans = cv.filter2D(src,3,myFilter)
+        print(trans)
+        return trans
+        # cv.imwrite("result.jpg",trans)
+        # print(trans)
+        # cv.imshow("result",trans)
+        # cv.waitKey()
 
     #自定义傅里叶变换,src为待操作图像
     def myFFT(self,src):
@@ -58,27 +76,33 @@ class MAIN():
 
     #图像混合操作
     def hybridImage(self,sigma):
-        trans1 = self.myFFT(self.img1)
-        trans2 = self.myFFT(self.img2)
-        h = trans1.shape[0]
-        w = trans1.shape[1]
-
-        #中心点坐标
-        center_w = w // 2
-        center_h = h // 2
-
-        #滤波操作
-        for i in range(int(center_h - sigma / 2 * h),int(center_h + sigma / 2 * h)):
-            for j in range(int(center_w - sigma / 2 * w),int(center_w + sigma / 2 * w)):
-                trans1[i][j] = [0,0,0]
-
-        for i in range(len(trans2)):
-            for j in range(len(trans2[0])):
-                if trans1[i][j][:].all() != 0:
-                    trans2[i][j] = [0,0,0]
-        img_back = self.myIFFT(trans1 + trans2)
-        print(img_back)
-        cv.imwrite("result.jpg",img_back)
+        trans1 = self.myGaussian(self.img1)
+        trans2 = self.img2 - self.myGaussian(self.img2)
+        result = trans2 + trans1
+        cv.imwrite("result.jpg",result)
+        # trans1 = self.myFFT(self.img1)
+        # trans2 = self.myFFT(self.img2)
+        # h = trans1.shape[0]
+        # w = trans1.shape[1]
+        #
+        # #中心点坐标
+        # center_w = w // 2
+        # center_h = h // 2
+        #
+        # #滤波操作
+        # for i in range(int(center_h - sigma / 2 * h),int(center_h + sigma / 2 * h)):
+        #     for j in range(int(center_w - sigma / 2 * w),int(center_w + sigma / 2 * w)):
+        #         trans1[i][j] = [0,0,0]
+        #
+        # for i in range(len(trans2)):
+        #     for j in range(len(trans2[0])):
+        #         if trans1[i][j][:].all() != 0:
+        #             trans2[i][j] = [0,0,0]
+        #
+        # #反傅里叶变换
+        # img_back = self.myIFFT(trans1 + trans2)
+        # print(img_back)
+        # cv.imwrite(OUTPUT,img_back)
 
 
 
--- temp.py
+++ temp.py
@@ -1,41 +1,20 @@
+import math
 import numpy as np
 
-from PIL import Image
-
-from numpy.fft import fft, ifft
-
+def get_cv(r, sigma):
+    return 1 / (2 * math.pi * sigma ** 2) * math.exp((-r**2) / (2 * sigma ** 2))
 
 
-def filterImage(srcImage):
-
-    # 打开图像文件并获取数据
-
-    srcIm = Image.open(srcImage)
-
-    srcArray = np.fromstring(srcIm.tobytes(), dtype=np.int8)
-
-
-
-    # 傅里叶变换并滤除低频信号
-
-    result = fft(srcArray)
-
-    result = np.where(np.absolute(result)<8e3, 0, result)
-
-    # 傅里叶反变换，保留实部
-
-    result = ifft(result)
-
-    result = np.int8(np.real(result))
+def get_window():
+    # 模糊半径为 2, sigma 为 1.5
+    radius, sigma = 2, 1.5
+    window = np.zeros((radius * 2 + 1, radius * 2 + 1))
+    for i in range(-radius, radius + 1):
+        for j in range(-radius, radius + 1):
+            r = (i ** 2 + j ** 2) ** 0.5
+            window[i + radius][j + radius] = get_cv(r, sigma)
+    return window / np.sum(window)
 
 
-
-    # 转换为图像
-
-    im = Image.frombytes(srcIm.mode, srcIm.size, result)
-
-    im.show()
-
-
-
-filterImage("./images/cat.png")
+if __name__ == '__main__':
+    print(get_window())
