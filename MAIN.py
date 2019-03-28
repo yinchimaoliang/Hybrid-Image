97a27d389b87e139dfd0d0d50b2d6c4a9cc25447
Yinhao Li <liyinhao0413@qq.com>
2019-03-28 16:42:39 +0800

    fix noisy points problem

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,6 @@
 import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
 
 
 
@@ -38,60 +37,50 @@ class MAIN():
     #自定义傅里叶变换,src为待操作图像
     def myFFT(self,src):
         #trans1盛放傅里叶变换后的结果
-
+        #逐通道进行傅里叶变换
         trans = np.zeros(src.shape,np.complex128)
-        # print(self.img1[:,:,0])
         for i in range(3):
             f = np.fft.fft2(src[:,:,i])
-            # print(f)
             trans[:,:,i] = f[:,:]
-            # print(trans[:,:,i])
         return trans
-        # print(trans[:,:,i])
-        # print(np.abs(img_back))
 
 
 
     def myIFFT(self,src):
-        # print("a",src)
-        return_val = np.zeros(src.shape,np.uint8)
+        # 务必用uint16否则出现噪点
+        return_val = np.zeros(src.shape,np.uint16)
+        #逐通道反傅里叶变换
         for i in range(3):
             f = np.fft.ifft2(src[:,:,i])
-            # print(f)
             return_val[:,:,i] = np.abs(f[:,:])
-            # print(src[:,:,i])
 
         return return_val
 
+    #图像混合操作
     def hybridImage(self,sigma):
-        # print(self.img1)
         trans1 = self.myFFT(self.img1)
         trans2 = self.myFFT(self.img2)
         h = trans1.shape[0]
         w = trans1.shape[1]
+
+        #中心点坐标
         center_w = w // 2
         center_h = h // 2
-        result = trans1[:,:,:]
-        for i in range(int(sigma * (center_h - h)),int(sigma * (center_h + h))):
-            for j in range(int(sigma * (center_w - w)),int(sigma * (center_w + w))):
-                result[i][j] = [0,0,0]
-                # print(result[i][j])
-                # print(i,j)
 
-        for i in range(len(result)):
-            for j in range(len(result[0])):
-                if result[i][j][:].all() == 0:
-                    result[i][j][:] = trans2[i][j][:]
-                # else:
-                #     result[i][j][:] = 0
+        #滤波操作
+        for i in range(int(center_h - sigma / 2 * h),int(center_h + sigma / 2 * h)):
+            for j in range(int(center_w - sigma / 2 * w),int(center_w + sigma / 2 * w)):
+                trans1[i][j] = [0,0,0]
+
+        for i in range(len(trans2)):
+            for j in range(len(trans2[0])):
+                if trans1[i][j][:].all() != 0:
+                    trans2[i][j] = [0,0,0]
+        img_back = self.myIFFT(trans1 + trans2)
+        print(img_back)
+        cv.imwrite("result.jpg",img_back)
 
 
-        print(trans1)
-        print(result)
-        img_back = self.myIFFT(trans1)
-        # print(img_back)
-        cv.imshow("img_back",img_back)
-        cv.waitKey()
 
 
 
@@ -99,6 +88,5 @@ class MAIN():
 if __name__ == '__main__':
     t = MAIN(SRC1,SRC2)
     t.loadImage()
-    t.hybridImage(0.02)
-    # t.myFFT(t.img1,80e3,1)
+    t.hybridImage(0.99)
 
